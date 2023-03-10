3dbf1cc6613df7b3c56833a4ecf65b47fa2e778e
NbMiranda <nathanbmiranda@hotmail.com>
2023-03-10 14:32:11 -0300

    Base imagem Laravel

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
-Route::apiResource('rental', 'App\Http\Controllers\RentalController');
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
